3dc8e05747c3d2dda57af0ebec534acc65ba92e8
Vaya0 <147645075+Vaya0@users.noreply.github.com>
2024-01-21 01:19:26 +0200

    Add files via upload
    Added functions Mark and Unmark.

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -1,6 +1,36 @@
-#include<iostream>
+/**
+*
+* Solution to course project # 05
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Vaya Yandina
+* @idnumber 5MI0600284
+* @compiler VC
+*
+* <предназначение на файла>
+*
+*/
+
+
+#include <iostream>
 
 constexpr int N = 10;
+
+void printField(char visibleValuesOfField[][N], int sizeOfField) {
+
+	for (int i = 0; i < sizeOfField; i++)
+	{
+		for (int j = 0; j < sizeOfField; j++)
+		{
+			std::cout << "{" << visibleValuesOfField[i][j] << "}";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
+}
 
 void isValidSizeOfField(int& sizeOfField) {
 	while (sizeOfField < 3 || sizeOfField>10) {
@@ -41,21 +71,21 @@ void isValidCoordinatesForMine(int& row, int& coll, int sizeOfField) {
 
 bool isAlreadyAssignedValue(char element)
 {
-	return element == '*';
+	return element == '*';//ako elementut veche e bomba
 }
 
 void placeMines(char playingField[][N], int sizeOfField, int numberOfMines) {
 
 	srand(time(0));
 
-	for (int i = numberOfMines; i > 0; i--) {
+	for (int i = 1; i <= numberOfMines; i++) {
 		int row = rand() % 10;
-		int coll = rand() % 10;
+		int coll = rand() % 10;// davame nqkakvi koordinati, no ne sme sigurnu che te shte popadnat v poleto
 
-		isValidCoordinatesForMine(row, coll, sizeOfField);
+		isValidCoordinatesForMine(row, coll, sizeOfField);// ako sa tvurde golemi produljavame da priemame novi i novi
 
-		if (isAlreadyAssignedValue(playingField[row][coll])) {
-			i++;
+		if (isAlreadyAssignedValue(playingField[row][coll])) {// dali veche e slojena mina, zashtoto random chisla moche da se povtarqt
+			i--;//podsigurqvame se che ako koordinatite sa bili ok, no veche e imalo, nqma da slojim vtora mina na sushtoto mqsto
 		}
 		else {
 			playingField[row][coll] = '*';
@@ -111,7 +141,7 @@ void countMinesAroundEachElement(char playingField[][N], int sizeOfField, int i,
 
 }
 
-void countMines(char playingField[][N], int sizeOfField) {
+void countMines(char playingField[][N], int sizeOfField) {//minavame prez tsqlata matritsa
 	for (int i = 0; i < sizeOfField; i++) {
 		for (int j = 0; j < sizeOfField; j++) {
 			if (playingField[i][j] != '*') {
@@ -121,28 +151,87 @@ void countMines(char playingField[][N], int sizeOfField) {
 	}
 }
 
-bool isValidUserInput(int x, int y, char command, int sizeOfField)
+bool isValidUserInput(int row, int coll, char command, int sizeOfField)
 {
 	return (command == '@' || command == '#' || command == '$')
-		&& x >= 0 && x < sizeOfField && y >= 0 && y < sizeOfField;
+		&& row >= 0 && row < sizeOfField && coll >= 0 && coll < sizeOfField;
 }
 
-//void createFunctionality(char visibleValuesOfField[][N], char playingField[][N], int sizeOfField, int numberOfMines) {
 
-//}
+void open(char visibleValuesOfField[][N], const char playingField[][N], int sizeOfField,
+			unsigned& movesLeft, int row, int coll, bool& gameOver) {
 
-void printField(char visibleValuesOfField[][N], int sizeOfField) {
 
-	for (int i = 0; i < sizeOfField; i++)
-	{
-		for (int j = 0; j < sizeOfField; j++)
-		{
-			std::cout << "{" << visibleValuesOfField[i][j] << "}";
-		}
-		std::cout << std::endl;
+}
+
+void mark(char visibleValuesOfField[][N], const char playingField[][N], int sizeOfField,
+			int row, int coll, unsigned numberOfFlagsLeft, int numberOfMines) {
+
+	if (visibleValuesOfField[row][coll] == '%') {//if marked
+		std::cout << "You have already marked this cell! ";
 	}
-	std::cout << std::endl;
+	else if (visibleValuesOfField[row][coll] != ' ') {//has the number of mines near it
+		std::cout << "You have already opened this cell! ";
+	}
+	else {
+		visibleValuesOfField[row][coll] = '%';//mark it
+		numberOfFlagsLeft--;
+		if (playingField[row][coll] == '*') {
+			numberOfMines--;
+		}
+	}
+}
 
+
+void unmark(char visibleValuesOfField[][N], const char playingField[][N], int sizeOfField,
+			int row, int coll, unsigned numberOfFlagsLeft, int numberOfMines) {
+	if (visibleValuesOfField[row][coll]  == '%') {
+		visibleValuesOfField[row][coll] = ' ';
+		numberOfFlagsLeft += 1;
+		if (visibleValuesOfField[row][coll] == ' ' && playingField[row][coll] == '*')
+			numberOfMines += 1;
+	}
+	else {
+		std::cout << "You cannot unmark a cell that has not been flagged as a possible mine yet!";
+	}
+}
+
+
+void createFunctionality(char visibleValuesOfField[][N], char playingField[][N], int sizeOfField, int numberOfMines) {
+	bool gameLost = false;
+	unsigned movesLeft = sizeOfField * sizeOfField - numberOfMines;
+	unsigned numberOfFlagsLeft = numberOfMines;
+	char command;
+	int row;
+	int coll;
+
+	do {//ako e s while ne mojem da ispolzvame promenlivite pri proverkata
+		std::cout << "Please enter the desired command {@, #, $} and the coordinates of the cell that You have chosen!: ";
+		std::cin >> command;
+		std::cin >> row;
+		std::cin >> coll;
+		if (isValidUserInput(row, coll, command, sizeOfField)) {
+			if (command == '@') {
+				open(visibleValuesOfField, playingField, sizeOfField, movesLeft, row, coll, gameLost);
+			}
+			else if (command == '#') {
+				mark(visibleValuesOfField, playingField, sizeOfField, row, coll, numberOfFlagsLeft, numberOfMines);
+			}
+			else if (command == '$') {
+				unmark(visibleValuesOfField, playingField, sizeOfField, row, coll, numberOfFlagsLeft, numberOfMines);
+			}
+			printField(visibleValuesOfField, sizeOfField);
+			if (!gameLost && !movesLeft)
+			{
+				std::cout << "Congratulations, You won !!! (^o^)";
+				gameLost = true;
+			}
+		}
+		if (!isValidUserInput(row, coll, command, sizeOfField)) {
+			std::cout << "Incorrect input!";
+		}
+	} while (!gameLost);
+	return;
 }
 
 int main() {
@@ -162,10 +251,10 @@ int main() {
 	fillPlayingFieldWithValues(visibleValuesOfField, '?');
 
 	placeMines(playingField, sizeOfField, numberOfMines);
-	printField(visibleValuesOfField, sizeOfField);
 	countMines(playingField, sizeOfField);
+	printField(visibleValuesOfField, sizeOfField);
 	std::cout << "If You want to open a cell, please enter: @." << std::endl;
 	std::cout << "If You want to mark a cell as a possible mine, please enter: #" << std::endl;
 	std::cout << "If You want to unmark said cell, please enter: $" << std::endl;
-	//createFunctionality(visibleValuesOfField, playingField, sizeOfField, numberOfMines);
+	createFunctionality(visibleValuesOfField, playingField, sizeOfField, numberOfMines);
 }
